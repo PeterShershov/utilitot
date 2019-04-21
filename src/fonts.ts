789f5341f476b1b960208878104127cd1013a01e
Peter Shershov <peters@wix.com>
2019-04-21 11:55:25 +0300

    changed file to environment and added font utils

---
+++ src/fonts.ts
@@ -0,0 +1,8 @@
+export function getActiveFont(fontFamily: string, fontSize: string = '12px') {
+    const fonts = fontFamily.split(',')
+
+    // experimental APIs are not included
+    // therefore `fonts.check` is not found on `Document` interface
+    // https://developer.mozilla.org/en-US/docs/Web/API/FontFaceSet/check
+    return fonts.find(font => (document as any).fonts.check(`${fontSize} ${font}`))
+}
